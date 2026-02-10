4758cc444a0b54d5d3681f8f34acdbb4647275f9
saraoskeirafaith-droid <saraos.keirafaith@gmail.com>
2026-02-10 11:59:19 +0800

    Create Personal Information.py
    Laboratory Activity
    
    Instruction: Create variables to store your personal information
    and display them in the following format:
    Fullname (String)
    Age (Integer)
    Height in meters (Float)
    Currently working? (Boolean)
    
    Then, display both the values and their data types using the print() and type() functions.

---
+++ Personal Information.py
@@ -0,0 +1,16 @@
+fullname = "Keira Faith T. Saraos"
+age = 18
+height = 1.55 
+currently_working = False
+
+print("Fullname:", fullname)
+print(type(fullname))
+
+print("Age:", age)
+print(type(age))
+
+print("Height in meters:", height)
+print(type(height))
+
+print("Currently working?", currently_working)
+print(type(currently_working))
